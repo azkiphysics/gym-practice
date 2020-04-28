669f26d825726e8836010d69089b572354f6d4dc
azkiphysics <kazu-physics6.97@fuji.waseda.jp>
2020-04-28 09:35:05 +0900

    update

---
+++ cart-pole/policy_gradient/pytorch/cart-pole_policy_gradient_pytorch.py
@@ -0,0 +1,18 @@
+import os
+import random
+from collections import namedtuple, deque
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def __main__ == "__name__":
+    pass
