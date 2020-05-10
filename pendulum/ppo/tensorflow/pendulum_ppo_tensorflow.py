fce6b37d0f82b86f1dacac030ee9ee82f93af366
azkiphysics <kazu-physics6.97@fuji.waseda.jp>
2020-05-10 12:02:20 +0900

    update

--- pendulum/ppo/tensorflow/pendulum_ppo_tensorflow.py
+++ pendulum/ppo/tensorflow/pendulum_ppo_tensorflow.py
@@ -1,4 +1,5 @@
 import os
+import re
 import random
 from collections import namedtuple, deque
 
@@ -10,7 +11,15 @@ import tensorflow as tf
 import tensorflow.keras as K
 
 
-Transition = namedtuple('Transition', ('s', 'a', 's_next', 'r', 'done'))
+EPISODE_SIZE = 10000
+EPOCH_SIZE = 10
+BATCH_SIZE = 64
+TRAJECTORY_SIZE = 2048
+GAMMA = 0.99
+LAMBDA = 0.95
+
+
+Transition = namedtuple('Transition', ('s', 'a', 'r', 'done'))
 
 
 class ReplayMemoryBuffer():
@@ -19,10 +28,13 @@ class ReplayMemoryBuffer():
         self.memory = deque(maxlen=buffer_size)
     
     def push(self, *args):
-        self.memory.append(*args)
+        self.memory.append(Transition(*args))
     
-    def sample(self, trajectory_size):
-        random.sample(self.memory, trajectory_size)
+    def get(self):
+        return self.memory
+    
+    def clear(self):
+        self.memory.clear()
     
     def __len__(self):
         return len(self.memory)
@@ -46,23 +58,19 @@ class Observer():
     
     def step(self, a):
         o_next, r, done, info = self._env.step(a[0])
-        return self.transform(o_next), self.transform(r, is_scaler=True)
-                self.transform(done, is_scaler=True), info
+        return self.transform(o_next), r, done, info
     
     def render(self):
         return self._env.render(mode="rgb_array")
     
-    def transform(self, x, is_scaler=False):
+    def transform(self, x):
         raise NotImplementedError("You have to implement transform method")
     
 
 class PendulumObserver(Observer):
 
-    def transform(self, x, is_scaler=False):
-        if is_scaler:
-            return np.array(x, dtype=np.float32).reshape=(1, -1)
-        else:
-            return np.reshape(x, (1, -1)).astype(np.float32)
+    def transform(self, x):
+        return np.reshape(x, (1, -1)).astype(np.float32)
 
 
 class Actor(K.Model):
@@ -70,10 +78,10 @@ class Actor(K.Model):
     def __init__(self, n_actions):
         super(Actor, self).__init__()
         normal = K.initializers.glorot_normal()
-        self.fc1 = K.layers.Dense(64, activation="relu", kernel_initializer=normal)
-        self.fc2 = K.layers.Dense(64, activation="relu", kernel_initializer=normal)
+        self.fc1 = K.layers.Dense(64, activation="tanh", kernel_initializer=normal)
+        self.fc2 = K.layers.Dense(64, activation="tanh", kernel_initializer=normal)
         self.mu = K.layers.Dense(n_actions, activation="tanh", kernel_initializer=normal)
-        self.logstd = np.zeros((n_actions,))
+        self.logstd = np.zeros((n_actions,)).astype(np.float32)
 
     def call(self, inputs):
         x = self.fc1(inputs)
@@ -116,13 +124,13 @@ class FNAgent():
         agent.initialized = True
         return agent
     
-    def initialize(self, experiences):
+    def initialize(self, optimizer):
         raise NotImplementedError("You have to implement initialize method.")
     
     def estimate(self, s):
         raise NotImplementedError("You have to implement estimate method.")
     
-    def update(self, experiences, gamma):
+    def update(self, experiences):
         raise NotImplementedError("You have to implement update method.")
 
     def policy(self, s):
@@ -170,34 +178,58 @@ class PPOAgent(FNAgent):
         agent.initialized = True
         return agent
 
-    def initialize(self, experiences):
-        pass
+    def initialize(self, optim_actor, optim_critic):
+        self.optim_actor = optim_actor
+        self.optim_critic = optim_critic
     
-    def estimate(self, s, s_next, r, gamma):
+    def estimate(self, s):
+        return self.critic(s)
+
+    def calc_logprob(self, s, a):
+        mu = self.actor(s)
+        logstd = self.actor.logstd * np.ones(mu.shape)
+        return -((a - mu)**2 )/ (2 * np.exp(logstd)) - np.log(np.sqrt(2 * np.exp(logstd)))
+    
+    def update(self, batch_s_v, batch_a_v, batch_adv_v, batch_ref_v, batch_old_logprob_v, eps):
+        with tf.GradientTape() as tape_critic:
+            val_v = self.estimate(batch_s_v)
+            loss_critic = tf.reduce_mean((val_v - tf.stop_gradient(batch_ref_v))**2)
         
-    
-    def update(self, batch, gamma):
-        batch_s = batch.s
-        batch_a = batch.a
-        batch_s_next = batch.s_next
-        batch_r = batch.r
-        batch_done = batch.done
+        grads_critic = tape_critic.gradient(loss_critic, self.critic.trainable_weights)
+        self.optim_critic.apply_gradients(zip(grads_critic, self.critic.trainable_weights))
+        
+        with tf.GradientTape() as tape_actor:
+            logprob_v = self.calc_logprob(batch_s_v, tf.stop_gradient(batch_a_v))
+            ratio_v = tf.exp(logprob_v - batch_old_logprob_v)
+            surr_obj_v = ratio_v * tf.stop_gradient(batch_adv_v)
+            c_ratio_v = tf.clip_by_value(ratio_v, 1-eps, 1+eps)
+            c_surr_obj_v = c_ratio_v * tf.stop_gradient(batch_adv_v)
+            loss_actor = -tf.math.segment_min([surr_obj_v, c_surr_obj_v], [0, 0])
+
+        grads_actor = tape_actor.gradient(loss_actor, self.actor.trainable_weights)
+        self.optim_actor.apply_gradients(zip(grads_actor, self.actor.trainable_weights))
+
 
     def policy(self, s):
          mu = self.actor(s)
-         a = np.random.normal(mu, sigma)
+         logstd = self.actor.logstd * np.ones(mu.shape)
+         a = np.random.normal(mu, np.exp(logstd)).astype(np.float32)
          return a
 
 
 class Trainer():
 
-    def __init__(self, buffer_size=10000, trajectory_size=2049, batch_size=64, gamma=0.99,
-                 gae_lambda=0.95, report_interval=10, log_dir=""):
+    def __init__(self, buffer_size=10000, trajectory_size=2049, epoch_size=10, batch_size=64, gamma=0.99,
+                 gae_lambda=0.95, lr_actor=1e-4, lr_critic=1e-3, eps=0.2, report_interval=10, log_dir=""):
         self.buffer_size = buffer_size
+        self.epoch_size = epoch_size
         self.batch_size = batch_size
         self.gamma = gamma
         self.trajectory_size = trajectory_size
-        self.gae_lambda = 0.95
+        self.gae_lambda = gae_lambda
+        self.lr_actor = lr_actor
+        self.lr_critic = lr_critic
+        self.eps = eps
         self.report_interval = report_interval
         self.logger = Logger(log_dir, self.trainer_name)
         self.experiences = ReplayMemoryBuffer(buffer_size)
@@ -212,6 +244,9 @@ class Trainer():
         return snaked
     
     def train_loop(self, env, agent, episode=200, initial_count=-1, render=False, observe_interval=0):
+        agent.initialize(optim_actor=K.optimizers.Adam(learning_rate=self.lr_actor),
+                         optim_critic=K.optimizers.Adam(learning_rate=self.lr_critic))
+        
         self.experiences = ReplayMemoryBuffer(self.buffer_size)
         self.reward_log = []
         frames = []
@@ -227,38 +262,67 @@ class Trainer():
                 a = agent.policy(s)
                 s_next, r, done, _ = env.step(a)
 
-                self.experiences.push(Transition(s, a, s_next, r, done))
+                self.experiences.push(s, a, r, done)
 
                 s = s_next
                 r_total += r
 
-                if len(self.experiences) < self.tragectory_size:
+                if len(self.experiences) < self.trajectory_size:
                     continue
 
-                transitions = self.experiences.sample(trajectory_size=self.trajectory_size)
-                trajectories = Transition(*zip(*transitions))
+                transitions = self.experiences.get()
+                trajs = Transition(*zip(*transitions))
 
-                self.step(agent, trajectories)
+                self.step(agent, trajs)
+
+                self.experiences.clear()
             else:
                 self.reward_log.append(r_total)
                 print("Episode: {}, Total Reward: {}".format(e, r_total))
     
-    def step(self, agent, trajectories):
-        
+    def step(self, agent, trajs):
+        raise NotImplementedError("You have to implement step method.")
 
 
 class PPOTrainer(Trainer):
 
-    def step(self, agent, experiences):
-        transitions = experiences.sample(self.batch_size)
-        batch = Transition(*zip(*transitions))
-        batch_s = np.vstack(batch.s)
-        batch_a = np.vstack(batch_a)
-        batch_s_next = np.vstack(batch.s_next)
-        batch_r = np.vstack(batch.r)
-        batch_not_done = 1 - np.vstack(batch.done)
+    def step(self, agent, trajs):
+        trajs_s_v = np.vstack(trajs.s)
+        trajs_a_v = np.vstack(trajs.a)
+        trajs_r_v = np.vstack(trajs.r)
+        trajs_done = np.array(trajs.done)
+        trajs_adv_v, trajs_ref_v = self.calc_adv(agent, trajs_s_v, trajs_r_v, trajs_done)
+        trajs_old_logprob_v = agent.calc_logprob(trajs_s_v, trajs_a_v)
 
-        agent.update(batch)
+        for epoch in range(self.epoch_size):
+            for i in range(0, len(trajs)-1, self.batch_size):
+                batch_s_v = trajs_s_v[i:i+self.batch_size]
+                batch_a_v = trajs_a_v[i:i+self.batch_size]
+                batch_r_v = trajs_r_v[i:i+self.batch_size]
+                batch_adv_v = trajs_adv_v[i:i+self.batch_size]
+                batch_ref_v = trajs_ref_v[i:i+self.batch_size]
+                batch_old_logprob_v = trajs_old_logprob_v[i:i+self.batch_size]
+
+                agent.update(batch_s_v, batch_a_v, batch_adv_v, batch_ref_v, batch_old_logprob_v, self.eps)
+    
+    def calc_adv(self, agent, trajs_s_v, trajs_r_v, trajs_done):
+        val_v = agent.estimate(trajs_s_v)
+        last_gae = np.array([0.0], dtype=np.float32)
+        adv_v = []
+        ref_v = []
+        for r, val, val_next, done in zip(reversed(trajs_r_v[:-1]), reversed(val_v[:-1]), reversed(val_v[1:]), reversed(trajs_done[:-1])):
+            if done:
+                delta = r - val
+                last_gae = delta
+            else:
+                delta = r + self.gamma * val_next - val
+                last_gae = delta + self.gamma * self.gae_lambda * last_gae
+            adv_v.append(last_gae)
+            ref_v.append(last_gae + val)
+        adv_v = np.array(list(reversed(adv_v)), dtype=np.float32)
+        ref_v = np.array(list(reversed(ref_v)), dtype=np.float32)
+
+        return adv_v, ref_v
     
 
 
@@ -270,7 +334,7 @@ class Logger():
             self.log_dir = os.path.join(os.path.dirname(os.getcwd()), "logs")
 
         if not os.path.exists(self.log_dir):
-            os.makedir(self.log_dir)
+            os.mkdir(self.log_dir)
         
         if dir_name:
             self.log_dir = os.path.join(self.log_dir, dir_name)
@@ -323,13 +387,15 @@ class PPOLogger(Logger):
     pass
 
 
-def main():
-    pass
+def main(env, agent, logger):
+    trainer = PPOTrainer()
+    trainer.train_loop(env, agent)
 
 if __name__ == "__main__":
     env = PendulumObserver(gym.make("Pendulum-v0"))
-    agent = PPOAgent()
+    n_actions = env.action_space.shape[0]
+    agent = PPOAgent(n_actions)
     logger = PPOLogger()
 
-    main()
+    main(env, agent, logger)
     
